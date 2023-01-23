bf1e49c30a828eeab856fe1290c3a8de9fc76bfb
simran mehra <simranmehra.iv@gmail.com>
2023-01-23 15:10:43 -0500

    Creating a table view

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class folder{
+    let title : String
+    
+    init(title: String) {
+        self.title = title
+      
+    }
+}
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return folder.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let categories = folder[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = folder[indexPath.row]
+        cell.textLabel?.textColor = .lightGray
+        cell.detailTextLabel?.textColor = .lightGray
+        cell.imageView?.image = UIImage(systemName: "folder")
+        cell.imageView?.tintColor = .orange
+        cell.selectionStyle = .none
+        return cell;
+    }
 
-	override func viewDidLoad() {
+    var folder = [
+    "groceries",
+    "shopping"
+    ]
+    @IBOutlet weak var AddButton: UIButton!
+    
+    @IBOutlet weak var SortMenu: UIButton!
+    
+    @IBOutlet weak var FolderTable: UITableView!
+    
+    
+    override func viewDidLoad() {
 		super.viewDidLoad()
+        FolderTable.delegate = self
+        FolderTable.dataSource = self
+        self.SortMenu.menu = menu
+        self.SortMenu.showsMenuAsPrimaryAction = true
 		// Do any additional setup after loading the view.
 	}
-
+   
 
 }
 
