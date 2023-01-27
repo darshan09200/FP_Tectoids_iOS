c60306cd30a33ae67950beadd7bec9b0e09b020a
pavitkalra150 <c0868150@mylambton.ca>
2023-01-26 19:57:20 -0500

    added actions on note screen on cell swipe and addbutton

---
+++ Notes/NotesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NotesTableViewCell.swift
+//  Notes
+//
+//  Created by PAVIT KALRA on 2023-01-23.
+//
+
+import UIKit
+
+class NotesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Notes/NotesViewController.swift
@@ -0,0 +1,120 @@
+//
+//  NotesViewController.swift
+//  Notes
+//
+//  Created by PAVIT KALRA on 2023-01-23.
+//
+
+import UIKit
+
+class NotesViewController: UIViewController {
+
+    private let searchController = UISearchController()
+    static let identifier = "NotesVC"
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var NotesCountLbl: UILabel!
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    lazy var addMenu = UIMenu(title: "", options: .displayInline, children: [
+        UIAction(title: "Add Task",
+               image: UIImage(systemName: "calendar.badge.plus")) { action in
+               // Perform action
+               },
+        UIAction(title: "Add Note",
+               image: UIImage(systemName: "note.text.badge.plus")) { action in
+               // Perform action
+               }
+    
+    ])
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addButton.showsMenuAsPrimaryAction = true
+
+        addButton.menu = addMenu
+        // Do any additional setup after loading the view.
+        configureSearchBar()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func addButton(_ sender: Any) {
+    }
+    
+    private func configureSearchBar() {
+        navigationItem.searchController = searchController
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+        searchController.delegate = self
+    }
+    
+    @IBAction func createNewNoteClicked(_ sender: UIButton) {
+        
+    }
+    
+    
+}
+
+
+extension NotesViewController: UISearchControllerDelegate, UISearchBarDelegate {
+    
+//
+    
+}
+
+extension NotesViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "note", for: indexPath)
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       //perform select function
+    }
+
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            deleteNote(at: indexPath)
+        }
+    }
+    func deleteNote(at indexPath: IndexPath) {
+   
+        //notes.remove(at: indexPath.row)
+        
+        tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let moveToCategory = UIContextualAction(style: .normal, title: "Move to Category") { (action, view, completion) in
+            // code to move the note to another category
+        }
+        moveToCategory.backgroundColor = .blue
+        moveToCategory.image = UIImage(systemName: "folder")
+
+        let delete = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completion) in
+                // Delete the note from your data source (e.g. an array of notes)
+                //tableView.deleteRows(at: [indexPath], with: .fade)
+                completion(true)
+            }
+            delete.image = UIImage(systemName: "trash")
+        let swipeActions = UISwipeActionsConfiguration(actions: [moveToCategory, delete])
+        return swipeActions
+    }
+}
